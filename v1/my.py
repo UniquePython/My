23c0eb7f5cc27e381feb7a6702cca43c50bd3361
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2024-09-03 22:39:25 +0530

    Added variable support

--- v1/my.py
+++ v1/my.py
@@ -12,6 +12,7 @@ class My:
         self.token_feed = self.tokens()
         self.returned_token = None
         self.stack = []
+        self.vars = {}
     
     def raise_error(self, message):
         raise Error(f"[{self.line_num},{self.col_num-1}]: {message}")
@@ -36,15 +37,15 @@ class My:
         for line in self.code.strip().split("\n"):
             self.line_num += 1
             self.col_num = 0
-            
             for token in line.strip().split(" "):
                 for _ in token:
                     self.col_num += 1
-                
-                if token == "print":
+                if token in ["print", "="]:
                     yield (token,)
                 elif token.isnumeric():
                     yield ("number", int(token))
+                elif token[0].isalpha():
+                    yield ("identifier", token)
                 else:
                     self.raise_error(f"Invalid token {token}")
             yield ("\n",)
@@ -61,8 +62,8 @@ class My:
         return True
     
     def parse_statement(self):
-        if not self.parse_print_statement():
-            self.raise_error('Expected print statement.')
+        if not self.parse_print_statement() and not self.parse_assignment():
+            self.raise_error('Expected: print statement or assignment.')
         token = self.next_token()
         if token[0] != '\n':
             self.raise_error('Expected end of line.')
@@ -75,18 +76,36 @@ class My:
             return False
         if not self.parse_expression():
             self.raise_error('Expected expression')
-
         value = self.stack_pop()
         print(value)
+        return True
+    
+    def parse_assignment(self):
+        token = self.next_token()
+        if token[0] != 'identifier':
+            self.return_token(token)
+            return False
+        identifier = token[1]
+        token = self.next_token()
+        if token[0] != '=':
+            self.raise_error('Expected: =')
+        if not self.parse_expression():
+            self.raise_error('Expected: expression')
+        self.vars[identifier] = self.stack_pop()
         return True
 
     def parse_expression(self):
         token = self.next_token()
-        if token[0] != 'number':
+        if token[0] not in ["number", "identifier"]:
             self.return_token(token)
             return False
-
-        self.stack_push(token[1])
+        if token[0] == 'identifier':
+            if token[1] not in self.vars:
+                self.raise_error(f"Syntax Error: Unknown variable '{token[1]}'")
+            else:
+                self.stack_push(self.vars[token[1]])
+        else:
+            self.stack_push(token[1])
         return True
     
     def run(self):
